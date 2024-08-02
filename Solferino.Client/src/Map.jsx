11587b907f76933452f9548d9a58166f53f3e7b9
ASSOBHEI, Kholoud <kholoud.assobhei@sogeti.com>
2024-08-02 14:59:48 +0200

    Can show markers for stations

--- Solferino.Client/src/Map.jsx
+++ Solferino.Client/src/Map.jsx
@@ -1,25 +1,41 @@
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Circle, LayerGroup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const SimpleMap = () => {
+    const [stations, setStations] = useState();
 
     const center = {
         latitude: 48.866667,
         longitude: 2.333333,
         name: "Paris",
     };
-    console.log(fetchData())
+    console.log(stations)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const stationMarkers = stations === undefined
+        ? <p>En cours de chargement </p>
+        : stations.map(station =>
+            <Marker key={station.name} position={[station.latitude, station.longitude]}> </Marker>
+        )
+        
+
+
+
 
     return (
-        <div>
-            <MapContainer center={[center.latitude, center.longitude]} zoom={9}  style={{ height: "80vh", width: "80vw" }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
-        </div>
+        <MapContainer center={[center.latitude, center.longitude]} zoom={9}  style={{ height: "80vh", width: "80vw" }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[center.latitude, center.longitude]}> </Marker>
+            {stationMarkers}
+        </MapContainer>
     );
 
 
@@ -31,7 +47,8 @@ const SimpleMap = () => {
             }
         });
         const data = await response.json();
-        return data;
+        setStations(data);
+        console.log(data);
     }
 };
 
